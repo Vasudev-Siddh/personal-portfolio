f7f0ab459e7eb54179eae77858acecb25783b833
Vasudev-Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-02-19 13:41:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db=client['portfolio']
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contactus',methods=['POST'])
+def contactus():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form.['message']
+
+    doc={"name":name,"email":email,"message":message}
+
+    db.contactus.insert_one(doc)
+    return "thank you for contacting us"
+
+app.run(debug=True)
